cd8cb0b6b61174079387f512317838117a35aa00
Gorbunova Ekaterina <login.kate1.pass@gmail.com>
2023-01-15 10:39:24 +0700

    Structure package, API & DB setup

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,17 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apikey: String
+    ) :ResponseBody
+
+    companion object {
+        const val API_KEY = "W6TSWLKU44ANYHLC"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
